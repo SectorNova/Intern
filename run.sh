3381e4e6c20d9ccd64fba1e82dcd6d205e21e83c
SectorNova <santhinaveen583@gmail.com>
2026-05-28 13:15:31 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install curl python3 python3-pip -y
-sudo apt-get install python3-flask
-flask --version
 
-echo "Testing GET"
+
+echo "============GET Output=============="
 curl http://127.0.0.1:5000/get
 
-echo -e "\nTesting POST"
+echo -e "\n===============POST Output==============="
 curl -X POST http://127.0.0.1:5000/post \
 -H "Content-Type: application/json" \
 -d '{"name":"Naveen"}'
 
-echo -e "\nTesting PUT"
+echo -e "\n===============PUT Output=================="
 curl -X PUT http://127.0.0.1:5000/put/0 \
 -H "Content-Type: application/json" \
 -d '{"name":"Arun"}'
 
-echo -e "\nTesting DELETE"
+echo -e "\n=================DELETE Output==================="
 curl -X DELETE http://127.0.0.1:5000/delete/0
 
 echo -e "\nDone ✅"
